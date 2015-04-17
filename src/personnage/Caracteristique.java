c42afbd9e1a3dd51f9987a98572f9ea563742cb9
armiakrajowa <beaudouink@gmail.com>
2015-04-17 02:14:31 +0200

    Ajout de la classe Caracteristique

---
+++ src/personnage/Caracteristique.java
@@ -0,0 +1,31 @@
+package personnage;
+
+public class Caracteristique {
+	
+	private int force;
+	private int agilite;
+	private int resistance;
+	
+
+	public int getForce () {
+		return this.force;
+	}
+	public int getAgilite () {
+		return this.agilite;
+	}
+	public int getResistance () {
+		return this.resistance;
+	}
+
+	public void setForce (int value) {
+		this.force = value;
+	}
+	public void setAgilite (int value) {
+		this.agilite = value;
+	}
+	public void setResistance (int value) {
+		this.resistance = value;
+	}
+	
+	
+}
