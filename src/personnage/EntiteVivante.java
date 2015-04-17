2093189f6d5046b5de0e170bfbe7b77cdc864405
armiakrajowa <beaudouink@gmail.com>
2015-04-17 13:13:23 +0200

    Suppression test.java

--- src/personnage/EntiteVivante.java
+++ src/personnage/EntiteVivante.java
@@ -5,9 +5,9 @@ public class EntiteVivante {
 	private Caracteristique caract = new Caracteristique();
 	private Inventaire invent = new Inventaire();
 	private String nom;
-	
-	
-	public void seDeplacer () {
+
+
+	public void seDeplacer (String ok) {
 		
 	}
 	public void attaquer () {
