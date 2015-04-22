50fc2761b4a14f73bddc5a5f8b104b9d5fd9b907
armiakrajowa <beaudouink@gmail.com>
2015-04-23 01:33:38 +0200

    Ajout des variable de classe pour la direction

--- src/personnage/EntiteVivante.java
+++ src/personnage/EntiteVivante.java
@@ -1,4 +1,7 @@
 package personnage;
+
+import carte.Carte;
+
 /**
  * Classe EntiteVivante, contient : <br>
  *  - Inventaire <br>
@@ -19,9 +22,15 @@ abstract public class EntiteVivante {
 	private Vie vie = new Vie();
 	private String nom;
 
+	private Carte carte = null;
+
 	private int positionX;
 	private int positionY;
 
+	public static final int BAS = 0;
+	public static final int GAUCHE = 1;
+	public static final int HAUT = 2;
+	public static final int DROITE = 3;
 
 	/**
 	 * Constructeur par defaut
@@ -29,12 +38,47 @@ abstract public class EntiteVivante {
 	public EntiteVivante () {
 
 	}
-	
+
 	/**
 	 * Gere le deplacement de l'entite
 	 */
-	abstract public void seDeplacer ();
-	
+	public void seDeplacer (int direct){
+		switch(direct){
+		case EntiteVivante.BAS :
+
+			this.setPositionY(this.getPositionY() - 1);
+			break;
+		case EntiteVivante.HAUT :
+			this.setPositionY(this.getPositionY() + 1);
+			break;
+		case EntiteVivante.GAUCHE :
+			this.setPositionX(this.getPositionX() - 1);
+			break;
+		case EntiteVivante.DROITE :
+			this.setPositionX(this.getPositionX() + 1);
+			break;
+		}
+	}
+
+	private boolean deplacementPossible (int direction) {
+		switch(direction){
+			case EntiteVivante.BAS :
+				this.setPositionY(this.getPositionY() - 1);
+				break;
+			case EntiteVivante.HAUT :
+				this.setPositionY(this.getPositionY() + 1);
+				break;
+			case EntiteVivante.GAUCHE :
+				this.setPositionX(this.getPositionX() - 1);
+				break;
+			case EntiteVivante.DROITE :
+				this.setPositionX(this.getPositionX() + 1);
+				break;
+			default : return false;
+		}
+	}
+
+
 	/**
 	 * Gere une attaque vers la cible de l'entite
 	 */
@@ -44,12 +88,12 @@ abstract public class EntiteVivante {
 		int paradeCible = cible.getInventaire().getParade();
 		int esquiveCible = Capacite.getRandomEsquive(adresseCible, encombrementCible, paradeCible);
 		System.out.println("esquive cible " + esquiveCible);
-		
+
 		int adresseJ = this.getCaracteristique().getAdresse();
 		int maniabiliteJ = this.getInventaire().getManiabilite();
 		int attaqueJ = Capacite.getRandomAttaque(adresseJ, maniabiliteJ);
 		System.out.println("attaque joueur " + attaqueJ);
-		
+
 		if (attaqueJ > esquiveCible){
 			int resistanceCible = cible.getCaracteristique().getResistance();
 			int protectionCible = cible.getInventaire().getProtection();
@@ -60,24 +104,24 @@ abstract public class EntiteVivante {
 			int impactJ = this.getInventaire().getImpact();
 			int degatJ = Capacite.getRandomDegat(forceJ, impactJ);
 			System.out.println("degat joueur " + degatJ);
-			
+
 			if (degatJ > defenseCible){
 				int degatSubit = degatJ - defenseCible;
 				System.out.println("degat subit " + degatSubit);
-				
+
 				int vieEnMoins = (degatSubit/3);
 				System.out.println("vie a retirer " + vieEnMoins);
-				
+
 				cible.getVie().retirerVie(vieEnMoins);
 			} else {
-				
+
 			}
-					
+
 		} else {
 
 		}
 	}
-	
+
 	/**
 	 * Retourne l'objet caracteristique de l'entite
 	 * @return Caracteristique
@@ -85,7 +129,7 @@ abstract public class EntiteVivante {
 	public Caracteristique getCaracteristique () {
 		return this.caracteristique;
 	}
-	
+
 	/**
 	 * Retourne l'objet inventaire de l'entite
 	 * @return Inventaire
@@ -93,7 +137,7 @@ abstract public class EntiteVivante {
 	public Inventaire getInventaire () {
 		return this.inventaire;
 	}
-	
+
 	/**
 	 * Retourne l'objet Vie de l'entite
 	 * @return Vie
@@ -101,16 +145,15 @@ abstract public class EntiteVivante {
 	public Vie getVie() {
 		return vie;
 	}
-	
+
 	/**
 	 * Retourne la position X de l'entite
 	 * @return positionX
 	 */
 	public int getPositionX() {
-		
 		return positionX;
 	}
-	
+
 	/**
 	 * Modifie la position X de l'entite
 	 * @param positionX
@@ -118,7 +161,7 @@ abstract public class EntiteVivante {
 	public void setPositionX(int positionX) {
 		this.positionX = positionX;
 	}
-	
+
 	/**
 	 * Retourne la position Y de l'entite
 	 * @return positionY
@@ -126,7 +169,7 @@ abstract public class EntiteVivante {
 	public int getPositionY() {
 		return positionY;
 	}
-	
+
 	/**
 	 * Modifie la position Y de l'entite
 	 * @param positionY
@@ -134,7 +177,7 @@ abstract public class EntiteVivante {
 	public void setPositionY(int positionY) {
 		this.positionY = positionY;
 	}
-	
+
 	/**
 	 * Retourne le nom de l'entite
 	 * @return nom
@@ -142,12 +185,19 @@ abstract public class EntiteVivante {
 	public String getNom() {
 		return nom;
 	}
-	
+
 	/**
 	 * Modifie le nom de l'entite
 	 * @param nom nom de l'entite
 	 */
 	public void setNom(String nom) {
 		this.nom = nom;
+	}
+
+	/**
+	 * @param carte the carte to set
+	 */
+	public void setCarte(Carte carte) {
+		this.carte = carte;
 	}
 }
