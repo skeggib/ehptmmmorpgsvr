49768c0a1aa31327f1b4558238510234addb80be
Sebastien Klasa <skeggib@gmail.com>
2015-04-23 20:05:24 +0200

    Ajout d'une carte de Test

--- src/affichage/Test.java
+++ src/affichage/Test.java
@@ -9,8 +9,8 @@ public class Test {
 	public static void main(String[] args) {
 		
 		Joueur joueur = new Joueur();
-		joueur.setPositionX(18);
-		joueur.setPositionY(18);
+		joueur.setPositionX(3);
+		joueur.setPositionY(3);
 		Carte carte = new Carte("");
 		InterfaceTerm inter = new InterfaceTerm();
 		inter.setJoueur(joueur);
