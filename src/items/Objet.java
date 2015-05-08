39e6fd07573212975eb3081c4468381709d9866b
armiakrajowa <beaudouink@gmail.com>
2015-05-08 17:54:52 +0200

    Classe Objet semi completer

--- src/items/Objet.java
+++ src/items/Objet.java
@@ -1,5 +1,30 @@
 package items;
 
 public abstract class Objet {
+	
+	/* 
+	 * Variable
+	 */
 
+	private String nom;
+	
+	/*
+	 * Constructeur
+	 */
+
+	public Objet () {
+		
+	}
+	
+	/*
+	 * Methode d'acces
+	 */
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 }
