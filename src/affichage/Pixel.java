e2e3db890f222df362dd9763826ba271a406afe7
Sebastien Klasa <skeggib@gmail.com>
2015-05-10 22:30:15 +0200

    Creation de la classe Pixel

---
+++ src/affichage/Pixel.java
@@ -0,0 +1,50 @@
+package affichage;
+
+public class Pixel {
+
+	char car;
+	
+	/**
+	 * Couleur du type :
+	 * "BLACK"
+	 * "RED"
+	 * "DEFAULT"
+	 * etc...
+	 */
+	String couleur;
+	
+	/**
+	 * Contructeur par defaut
+	 */
+	public Pixel() {
+		this.car = ' ';
+		this.couleur = "DEFAULT";
+	}
+	
+	/**
+	 * Constructeur par parametres 
+	 * @param car Caractere
+	 * @param couleur Couleur
+	 */
+	public Pixel(char car, String couleur) {
+		this.car = car;
+		this.couleur = couleur;
+	}
+	
+	public void setCar(char car) {
+		this.car = car;
+	}
+	
+	public char getCar() {
+		return this.car;
+	}
+	
+	public String getCouleur() {
+		// TODO: A faire
+		return "";
+	}
+	
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+}
