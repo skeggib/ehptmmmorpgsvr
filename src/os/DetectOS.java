701fda745c761a51ef6e89f8857b51b2a7830f38
Sebastien Klasa <skeggib@gmail.com>
2015-05-10 22:47:43 +0200

    Ajout de la classe DetectOS

---
+++ src/os/DetectOS.java
@@ -0,0 +1,25 @@
+package os;
+
+public class DetectOS {
+
+	private static String OS = System.getProperty("os.name").toLowerCase();
+	
+	private DetectOS() {}
+	
+	public static boolean Windows() {
+		return (OS.indexOf("win") >= 0);
+	}
+ 
+	public static boolean Mac() {
+		return (OS.indexOf("mac") >= 0);
+	}
+ 
+	public static boolean Unix() {
+		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
+	}
+ 
+	public static boolean Solaris() {
+		return (OS.indexOf("sunos") >= 0);
+	}
+	
+}
