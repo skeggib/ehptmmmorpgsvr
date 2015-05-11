d59c13e5c975d495d23c984d8ed0c8facd477d99
Sebastien Klasa <skeggib@gmail.com>
2015-05-11 21:19:17 +0200

    Ajout des methodes d'ajout/changement/suppression de contenu de Case

--- src/carte/Case.java
+++ src/carte/Case.java
@@ -7,12 +7,44 @@ public class Case {
 	public Case() {
 		contenu = null;
 	}
+	
+	/**
+	 * Supprime le contenu de la case
+	 */
+	public void supprContenu() {
+		this.setContenu(null);
+	}
+	
+	/**
+	 * Ajoute un contenu a la case, a condition qu'elle soit vide
+	 * @param nouvContenu Contenu a ajouter
+	 * @return True si le contenu a ete ajoute
+	 */
+	public boolean ajoutContenu(ContenuCase nouvContenu) {
+		// On ne change rien s'il y a déjà quelque chose
+		if (this.getContenu() != null)
+			return false;
+		
+		this.setContenu(nouvContenu);
+		return true;
+	}
+	
+	/**
+	 * Change le contnu de la case
+	 * @param nouvContenu Nouveau contenu de la case
+	 * @return Le contenu precedent de la case
+	 */
+	public ContenuCase changerContenu(ContenuCase nouvContenu) {
+		ContenuCase rtrn = this.getContenu();
+		this.setContenu(nouvContenu);
+		return rtrn;
+	}
 
 	public ContenuCase getContenu() {
 		return contenu;
 	}
 
-	public void setContenu(ContenuCase contenu) {
+	private void setContenu(ContenuCase contenu) {
 		this.contenu = contenu;
 	}
 	
