4896bbeb59013d11e2cc09c51b2d1e5adcbdbe96
Sebastien Klasa <skeggib@gmail.com>
2015-05-11 03:12:12 +0200

    Creation de la classe Case

--- src/carte/Case.java
+++ src/carte/Case.java
@@ -2,4 +2,18 @@ package carte;
 
 public class Case {
 
+	private ContenuCase contenu;
+	
+	public Case() {
+		contenu = null;
+	}
+
+	public ContenuCase getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(ContenuCase contenu) {
+		this.contenu = contenu;
+	}
+	
 }
