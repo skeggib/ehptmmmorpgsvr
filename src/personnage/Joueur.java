1b39ce3d19fe58cb6a2ac514d8f9c250b23d1b9c
armiakrajowa <beaudouink@gmail.com>
2015-05-12 21:12:49 +0200

    Ajout des constructeurs de la classe Joueur

--- src/personnage/Joueur.java
+++ src/personnage/Joueur.java
@@ -1,9 +1,31 @@
 package personnage;
 
+import java.util.Scanner;
+
 import carte.ContenuCase;
 
 public class Joueur extends EntiteVivante {
 	
+	
+	/*
+	 * Constructeur
+	 */
+	
+	@SuppressWarnings("resource")
+	public Joueur () {
+		super();
+		this.setNom(new Scanner(System.in).nextLine());
+	}
+	
+	public Joueur (String nom) {
+		super();
+		this.setNom(nom);
+	}
+	
+	public Joueur (String nom, int force, int adresse, int resistance){
+		super(force, adresse, resistance);
+		this.setNom(nom);
+	}
 	
 	/*
 	 * Methodes
