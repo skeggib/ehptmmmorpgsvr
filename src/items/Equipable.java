292b9aa68c1a197ec7e16589a67b11c2da1f9d04
armiakrajowa <beaudouink@gmail.com>
2015-05-12 21:34:42 +0200

    Ajout de la qualite sur les objets Equipables

--- src/items/Equipable.java
+++ src/items/Equipable.java
@@ -1,11 +1,37 @@
 package items;
 
+import java.util.Random;
+
 public abstract class Equipable extends Objet{
 	
+	/*
+	 * Constantes
+	 */
+	
+	private final static int MAUVAIS = 0;
+	private final static int COMMUN = 1;
+	private final static int INHABITUEL = 2;
+	private final static int RARE = 3;
+	private final static int EPIQUE = 4;
+	private final static int LEGENDAIRE = 5;
 	/*
 	 * Variables
 	 */
 	
-	private int qualite;
-
+	public Equipable () {
+		int resultRand = new Random().nextInt(1001);
+		if(resultRand < 200){
+			this.setQualite(Equipable.MAUVAIS);
+		} else if (resultRand >= 200 && resultRand < 600){
+			this.setQualite(Equipable.COMMUN);
+		} else if (resultRand >= 600 && resultRand < 850){
+			this.setQualite(Equipable.INHABITUEL);
+		} else if (resultRand >= 850 && resultRand < 950){
+			this.setQualite(Equipable.RARE);
+		} else if (resultRand >= 950 && resultRand < 1000){
+			this.setQualite(Equipable.EPIQUE);
+		} else if (resultRand == 1000){
+			this.setQualite(Equipable.LEGENDAIRE);
+		}
+	}
 }
