2fd1dac6b7cff5ea7915686850b6e9bba5ce9595
armiakrajowa <beaudouink@gmail.com>
2015-05-12 21:12:24 +0200

    Ajout de TODO (important!)

--- src/personnage/Equipement.java
+++ src/personnage/Equipement.java
@@ -1,5 +1,5 @@
 package personnage;
 
 public class Equipement extends ContenantObjet{
-
+	//TODO Redefinir les methode de ContenantObjet ici pour verifier qu'il n'y a qu'un seul objet de chaque type (1 casque, 1 torse, etc)
 }
