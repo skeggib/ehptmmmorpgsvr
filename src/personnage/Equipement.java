62a1fda3fcc0fec2e742a49005dbaaad9985a2db
armiakrajowa <beaudouink@gmail.com>
2015-05-12 14:51:55 +0200

    Transformation de l'interface ContenirObjets en classe ContenantObjets

--- src/personnage/Equipement.java
+++ src/personnage/Equipement.java
@@ -1,5 +1,5 @@
 package personnage;
 
-public class Equipement {
+public class Equipement extends ContenantObjet{
 
 }
--- src/personnage/Inventaire.java
+++ src/personnage/Inventaire.java
@@ -1,5 +1,5 @@
 package personnage;
 
-public class Inventaire {
+public class Inventaire extends ContenantObjet{
 
 }
