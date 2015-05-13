32a61d33e7776d68b8cdf1cee63850cdcfc4a836
armiakrajowa <beaudouink@gmail.com>
2015-05-13 19:54:16 +0200

    Implementation de la methode "ajouterObjet" de la classe Equipement

--- src/personnage/Equipement.java
+++ src/personnage/Equipement.java
@@ -2,50 +2,62 @@ package personnage;
 
 import java.util.List;
 
+import items.Arme;
 import items.Equipable;
 import items.Objet;
 
-public class Equipement implements ContenirObjets{
-	
+public class Equipement implements ContenirObjets {
+
 	private List<Equipable> listObjet;
-	
-	public Objet getObjet(int index){
+
+	public Objet getObjet(int index) {
 		return this.listObjet.get(index);
 	}
 
-	public boolean ajouterObjet(Objet o) {
-		if(o instanceof Equipable){
-			Equipable eq = (Equipable) o;
-			
-			for(int i = 0; i < this.getTaille(); i++){
-				if(this.listObjet.get(i) == eq){
-					
+	public boolean ajouterObjet(Objet obj) {
+		if (obj instanceof Equipable) {
+			Equipable eq = (Equipable) obj;
+			if (!this.listObjet.contains(eq)) {
+				if (eq instanceof Arme) {
+					int nombreArmeEquipe = 0;
+					for (int i = 0; i < this.listObjet.size(); i++) {
+						if (this.listObjet.get(i) instanceof Arme) {
+							nombreArmeEquipe++;
+						}
+					}
+					if (nombreArmeEquipe < 2) {
+						this.listObjet.add(eq);
+						return true;
+					}
+				} else {
+					boolean dejaEquipe = false;
+					for (int i = 0; i < this.listObjet.size(); i++) {
+						if (this.listObjet.get(i).getClass() == eq.getClass()) {
+							dejaEquipe = true;
+						}
+					}
+					if (!dejaEquipe) {
+						this.listObjet.add(eq);
+						return true;
+					}
 				}
 			}
-			
-			
-			
-			if (!(this.listObjet.contains(eq))) {
-				this.listObjet.add(eq);
-				if (this.listObjet.contains(eq)) {
-					return true;
-				}
-			}
+			return false;
 		}
 		return false;
 	}
 
 	public boolean retirerObjet(Objet o) {
-		if(this.listObjet.contains(o)){
+		if (this.listObjet.contains(o)) {
 			this.listObjet.remove(o);
-			if(!(this.listObjet.contains(o))){
+			if (!(this.listObjet.contains(o))) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public int getTaille () {
+
+	public int getTaille() {
 		return this.listObjet.size();
 	}
 }
