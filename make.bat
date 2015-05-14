63f328f9ccd61f227758ee7b37ff2ecc24d2a7c8
Sebastien Klasa <skeggib@gmail.com>
2015-05-14 23:34:53 +0200

    Modification de make.bat (compilation plus rapide)

--- make.bat
+++ make.bat
@@ -1,1 +1,17 @@
-for /R %%f in (*.java) do javac "%%f"
+@echo off
+
+echo Recherche...
+for /R %%i in (*.java) do (
+	call :AddFile "%%i"
+)
+
+echo Compilation...
+javac %a%
+
+echo Fini !
+goto End
+
+:AddFile
+	set a=%a% %1
+
+:End
