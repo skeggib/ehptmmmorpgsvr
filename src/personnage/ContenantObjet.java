e6d7d5001aec7f24c3b97d6969b79d13e149f84f
armiakrajowa <beaudouink@gmail.com>
2015-05-16 21:11:59 +0200

    Correction de la methode ajouterObjet

--- src/personnage/ContenantObjet.java
+++ src/personnage/ContenantObjet.java
@@ -1,38 +1,54 @@
 package personnage;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import items.Objet;
 
-public class ContenantObjet implements ContenirObjets {
-	
+public abstract class ContenantObjet implements ContenirObjets {
+
 	private List<Objet> listObjet;
+
+	/*
+	 * Constructeur
+	 */ 
 	
-	public Objet getObjet(int index){
+	public ContenantObjet() {
+		this.listObjet = new LinkedList<Objet>();
+	}
+
+	/*
+	 * Methode
+	 */
+	
+	public Objet getObjet(int index) {
 		return this.listObjet.get(index);
 	}
 
 	public boolean ajouterObjet(Objet o) {
-		if(!(this.listObjet.contains(o))){
+		if (this.listObjet.contains(o)) {
+			return false;
+		} else {
 			this.listObjet.add(o);
-			if(this.listObjet.contains(o)){
+			return true;
+		}
+	}
+
+	public boolean retirerObjet(Objet o) {
+		if (this.listObjet.contains(o)) {
+			this.listObjet.remove(o);
+			if (!(this.listObjet.contains(o))) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	public boolean retirerObjet(Objet o) {
-		if(this.listObjet.contains(o)){
-			this.listObjet.remove(o);
-			if(!(this.listObjet.contains(o))){
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public int getTaille () {
+	public int getTaille() {
 		return this.listObjet.size();
+	}
+
+	public boolean contient(Objet o) {
+		return this.listObjet.contains(o);
 	}
 }
