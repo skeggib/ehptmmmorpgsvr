6b7a3d12d6d68b313e081b400a01aa12d4057089
Sebastien Klasa <skeggib@gmail.com>
2015-05-20 23:05:38 +0200

    Ajout de la classe FenetreInfosJoueurs

--- src/affichage/fenetre/Test.java
+++ src/affichage/fenetre/Test.java
@@ -1,36 +1,15 @@
 package affichage.fenetre;
 
-import jeu.Log;
+import personnage.Joueur;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		FenetreLog fen = new FenetreLog(20, 10);
-		Log log = new Log();
+		Joueur joueur = new Joueur("Name");
 		
-		log.add("Salut 1");
-		log.add("Salut 2");
-		log.add("Salut 3");
-		log.add("Salut 4");
-		log.add("Salut 5");
-		log.add("Salut 6");
-		log.add("Salut 7");
-		log.add("Salut 8");
-		log.add("Salut 9");
-		log.add("Salut 10");
-		log.add("Salut 11");
-		log.add("Salut 12");
-		log.add("Salut 13");
-		log.add("Salut 14");
-		log.add("Salut 15");
-		
-		fen.setLog(log);
-		
-		fen.getMatriceFen().afficher();
-		
-		log.add("Test");
-		log.add("Message");
+		FenetreInfosJoueur fen = new FenetreInfosJoueur(20, 20);
+		fen.setJoueur(joueur);
 		
 		fen.getMatriceFen().afficher();
 
