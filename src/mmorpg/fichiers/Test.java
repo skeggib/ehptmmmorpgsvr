346916bb2831660a8c772905730341ab062892d2
armiakrajowa <beaudouink@gmail.com>
2015-06-03 22:24:31 +0200

    Ajout du code pour charger un objet dans mmorpg.fichier.Test

--- src/mmorpg/fichiers/Test.java
+++ src/mmorpg/fichiers/Test.java
@@ -2,19 +2,18 @@ package mmorpg.fichiers;
 
 import java.io.File;
 
+import mmorpg.jeu.Moteur;
+
 public class Test {
 
 	public static void main(String[] args) {
-		String chemin = "ressources/noms/arme.txt";
-		File f = new File(chemin);
-
-		System.out.println("Chemin absolu du fichier : " + f.getAbsolutePath());
-		System.out.println("Nom du fichier : " + f.getName());
-		System.out.println("Est-ce qu'il existe ? " + f.exists());
-		System.out.println("Est-ce un répertoire ? " + f.isDirectory());
-		System.out.println("Est-ce un fichier ? " + f.isFile());
-
-		System.out.println(LectureFichier.lireS(chemin));
+		Object obj = LectureObjet.lire("objet.ser");
+		
+		System.out.println(obj.getClass());
+		
+		Moteur vroum = (Moteur) obj;
+		
+		vroum.jouer();
 	}
 
 }
