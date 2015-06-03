8629a574ed388e486f66c3dfd37c91290b73e381
armiakrajowa <beaudouink@gmail.com>
2015-06-03 22:38:05 +0200

    Test EcritureObjet

--- src/mmorpg/fichiers/Test.java
+++ src/mmorpg/fichiers/Test.java
@@ -2,18 +2,35 @@ package mmorpg.fichiers;
 
 import java.io.File;
 
+import mmorpg.items.Arme;
 import mmorpg.jeu.Moteur;
+import mmorpg.personnage.Joueur;
 
 public class Test {
 
 	public static void main(String[] args) {
+		
+		Joueur j = new Joueur("Armya", 20, 10, 10);
+		j.rammasserObjet(new Arme());
+		
+		EcritureObjet.ecrire(j);
+		
 		Object obj = LectureObjet.lire("objet.ser");
 		
 		System.out.println(obj.getClass());
 		
-		Moteur vroum = (Moteur) obj;
+		Joueur j2 = (Joueur) obj;
+
+		System.out.println("FORCE (BASE) : " + j.getCaractPrinc().getForce());
+		System.out.println("FORCE (SAVE) : " + j2.getCaractPrinc().getForce());
+
+		System.out.println("NOM ARME (BASE) : " + j.getInventaire().getObjet(0).getNom());
+		System.out.println("NOM ARME (SAVE) : " + j2.getInventaire().getObjet(0).getNom());
 		
-		vroum.jouer();
+		//
+		// Moteur vroum = (Moteur) obj;
+		//
+		// vroum.jouer();
 	}
 
 }
