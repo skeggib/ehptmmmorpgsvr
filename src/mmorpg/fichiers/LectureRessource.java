05806642d120b2e5d1d73a42179a5bd3655a4c73
Sebastien Klasa <skeggib@gmail.com>
2015-06-04 20:16:02 +0200

    Ajout de LectureRessource.lireTab

--- src/mmorpg/fichiers/LectureRessource.java
+++ src/mmorpg/fichiers/LectureRessource.java
@@ -1,6 +1,7 @@
 package mmorpg.fichiers;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public abstract class LectureRessource {
 
@@ -41,6 +42,43 @@ public abstract class LectureRessource {
 		}
 
 		return rtrn;
+	}
+	
+	public static String[] lireTab(String path) {
+		ArrayList<String> array = new ArrayList<String>();
+
+		InputStream is = Object.class
+				.getResourceAsStream("/ressources/" + path);
+
+		// Si le fichier est introuveble dans le jar
+		if (is == null) {
+
+			// On lit celui qui est dans le dossier ressources
+			return LectureFichier.lireT("ressources/" + path);
+		}
+
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		String line;
+
+		try {
+			while ((line = br.readLine()) != null) {
+				array.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			br.close();
+			isr.close();
+			is.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return array.toArray(new String[array.size()]);
 	}
 
 }
--- src/mmorpg/fichiers/Test.java
+++ src/mmorpg/fichiers/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println(LectureRessource.lire("carte/test"));
+		System.out.println(LectureRessource.lireTab("carte/test").length);
 		
 	}
 
