c781e0f7f980513af0abcfbdf3a37f3b93c173c2
armiakrajowa <beaudouin@gmail.com>
2015-06-11 03:50:13 +0200

    Reorganisation des imports

--- src/mmorpg/fichiers/LectureFichier.java
+++ src/mmorpg/fichiers/LectureFichier.java
@@ -3,7 +3,6 @@ package mmorpg.fichiers;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 
 public abstract class LectureFichier {
 	/*
--- src/mmorpg/fichiers/LectureRessource.java
+++ src/mmorpg/fichiers/LectureRessource.java
@@ -1,6 +1,9 @@
 package mmorpg.fichiers;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public abstract class LectureRessource {
